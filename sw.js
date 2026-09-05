6db25b3a4625d3e0208a3d337d183c367f4544fd
BushTrophyRoom <raythoroughgood@gmail.com>
2026-09-06 00:03:00 +1000

    Refresh app cache for customer quote responses

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'tradie-quote-buddy-v13';
+const CACHE_NAME = 'tradie-quote-buddy-v14';
 const APP_SHELL = [
   './',
   './index.html',
-  './app-v7.js?v=9',
-  './response-actions.js?v=1',
-  './quote-terms.js',
+  './app-v7.js?v=10',
+  './response-actions.js?v=2',
+  './quote-terms.js?v=2',
+  './respond.html',
   './manifest.webmanifest?v=9',
   './icon.svg'
 ];
@@ -35,7 +36,7 @@ self.addEventListener('fetch', event => {
         var url = new URL(event.request.url);
         if (url.origin === self.location.origin && url.pathname.endsWith('/app-v7.js')) {
           return response.text().then(function(text) {
-            var injected = text + "\n(function(){var s=document.createElement('script');s.src='./quote-terms.js';s.defer=false;document.head.appendChild(s);})();\n(function(){var s=document.createElement('script');s.src='./response-actions.js?v=1';s.defer=false;document.head.appendChild(s);})();\n";
+            var injected = text + "\n(function(){var s=document.createElement('script');s.src='./quote-terms.js?v=2';s.defer=false;document.head.appendChild(s);})();\n(function(){var s=document.createElement('script');s.src='./response-actions.js?v=2';s.defer=false;document.head.appendChild(s);})();\n";
             return new Response(injected, {headers:{'Content-Type':'application/javascript; charset=utf-8'}});
           });
         }

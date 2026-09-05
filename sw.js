eab81a4b1105d7b19671c67ebeee62a472ac6a04
BushTrophyRoom <raythoroughgood@gmail.com>
2026-09-05 22:04:39 +1000

    Cache the mobile app shell and subscription page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='tqb-shell-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='tqb-shell-v2';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg','./subscription.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{

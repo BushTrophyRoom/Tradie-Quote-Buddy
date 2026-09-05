14c5cab6fa18f5d25aac509ada749d13df465fc2
BushTrophyRoom <raythoroughgood@gmail.com>
2026-09-05 22:08:36 +1000

    Fix app cache so latest code loads

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tqb-shell-v2';
+const CACHE='tqb-shell-v3';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg','./subscription.html'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
@@ -6,7 +6,10 @@ self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   const url=new URL(event.request.url);
   if(url.origin!==location.origin) return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-    const copy=response.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return response;
-  }).catch(()=>caches.match('./index.html'))));
+  const isAppFile=['/index.html','/app.js','/sw.js','/manifest.webmanifest'].some(p=>url.pathname.endsWith(p));
+  if(isAppFile){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))));
 });

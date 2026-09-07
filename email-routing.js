073d57709545ff62285cba386e49b33af1ec0d90
Ray T <raythoroughgood@gmail.com>
2026-09-07 13:05:35 +1000

    Add invoice/quote email routing flags

--- email-routing.js
+++ email-routing.js
@@ -3,12 +3,17 @@
   var originalSend=window.emailjs.send.bind(window.emailjs);
   window.emailjs.send=function(serviceId,templateId,params,options){
     params=params||{};
-    if(templateId==='template_9kx2gib' && !params.email_subject){
-      var business=params.business_name||'Tradie Quote Buddy';
-      if(params.invoice_number){
-        params.email_subject='Invoice '+params.invoice_number+' from '+business;
-      }else if(params.quote_number){
-        params.email_subject='Quote '+params.quote_number+' from '+business;
+    if(templateId==='template_9kx2gib'){
+      var isInvoice=!!params.invoice_number;
+      params.is_invoice=isInvoice;
+      params.email_type=isInvoice?'invoice':'quote';
+      if(!params.email_subject){
+        var business=params.business_name||'Tradie Quote Buddy';
+        if(isInvoice){
+          params.email_subject='Invoice '+params.invoice_number+' from '+business;
+        }else if(params.quote_number){
+          params.email_subject='Quote '+params.quote_number+' from '+business;
+        }
       }
     }
     return originalSend(serviceId,templateId,params,options);

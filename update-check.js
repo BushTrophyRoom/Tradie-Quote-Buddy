cd1b6e41956f8d823cdb58dcca2c17bc4727d13d
Ray T <raythoroughgood@gmail.com>
2026-09-10 17:43:39 +1000

    Load invoice fix on every app start

--- update-check.js
+++ update-check.js
@@ -1,12 +1,14 @@
 (function(){
   'use strict';
   var KEY='dustyboots-build-seen';
-  var BUILD='2026-09-10-1';
-  try{
-    if(localStorage.getItem(KEY)!==BUILD) localStorage.setItem(KEY,BUILD);
-  }catch(e){}
+  var BUILD='2026-09-10-2';
+  try{localStorage.setItem(KEY,BUILD)}catch(e){}
+  var sc=document.createElement('script');
+  sc.src='./invoice-fix.js?v=1';
+  sc.defer=true;
+  document.head.appendChild(sc);
   if(!('serviceWorker' in navigator)) return;
-  navigator.serviceWorker.register('./sw.js?v=59',{updateViaCache:'none'}).then(function(reg){
-    try{ return reg.update(); }catch(e){}
+  navigator.serviceWorker.register('./sw.js?v=60',{updateViaCache:'none'}).then(function(reg){
+    try{return reg.update()}catch(e){}
   }).catch(function(){});
 })();

66e8aef347afc0aa6cb27dd51ac2dc4f72cb2f49
Ray T <raythoroughgood@gmail.com>
2026-09-11 09:26:14 +1000

    Force service worker update and fresh invoice fix

--- update-check.js
+++ update-check.js
@@ -1,14 +1,18 @@
 (function(){
   'use strict';
   var KEY='dustyboots-build-seen';
-  var BUILD='2026-09-10-2';
+  var BUILD='2026-09-11-1';
   try{localStorage.setItem(KEY,BUILD)}catch(e){}
   var sc=document.createElement('script');
-  sc.src='./invoice-fix.js?v=1';
+  sc.src='./invoice-fix.js?v=65';
   sc.defer=true;
   document.head.appendChild(sc);
   if(!('serviceWorker' in navigator)) return;
-  navigator.serviceWorker.register('./sw.js?v=60',{updateViaCache:'none'}).then(function(reg){
+  navigator.serviceWorker.register('./sw.js?v=70',{updateViaCache:'none'}).then(function(reg){
+    if(reg.waiting){try{reg.waiting.postMessage({type:'SKIP_WAITING'})}catch(e){}}
     try{return reg.update()}catch(e){}
   }).catch(function(){});
+  navigator.serviceWorker.addEventListener('controllerchange',function(){
+    try{window.location.reload()}catch(e){}
+  });
 })();
